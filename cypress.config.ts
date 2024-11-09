ebfb4f9b0d470a829d1210dcf049036e59cc1f6a
orhunakkan <orhunakkan@outlook.com>
2024-11-09 05:06:03 -0600

    first commit for portfolio

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
 export default defineConfig({
-  e2e: {
-    specPattern: 'tests/**/*.cy.ts',
-    supportFile: 'utilities/e2e.ts',
-    setupNodeEvents(on, config) {
+    e2e: {
+        specPattern: 'tests/**/*.cy.ts',
+        supportFile: 'utilities/e2e.ts',
+        setupNodeEvents(on, config) {
+        },
     },
-  },
 });
