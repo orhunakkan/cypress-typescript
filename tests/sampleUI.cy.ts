fd75b0d46b5a3a4f22f2e85dd36f0e709111c12b
orhunakkan <orhunakkan@outlook.com>
2024-11-22 10:54:27 -0600

    Small changes to test file

--- tests/sampleUI.cy.ts
+++ tests/sampleUI.cy.ts
@@ -1,22 +1,18 @@
 import {SamplePage} from "../pages/samplePage";
 
+const baseURL = 'https://formy-project.herokuapp.com/';
+let samplePage: SamplePage;
+
 describe('Formy Complete Web Form', () => {
 
-    const baseURL = 'https://formy-project.herokuapp.com/';
-    let samplePage: SamplePage;
-
-    before(() => {
-        samplePage = new SamplePage();
-    });
-
     it('Submit Webform and Validate', () => {
+        samplePage = new SamplePage();
         cy.visit(`${baseURL}form`);
         cy.get(samplePage.firstNameInput).type('John');
         cy.get(samplePage.lastNameInput).type('Doe');
         cy.get(samplePage.jobTitleInput).type('QA Engineer');
         cy.get(samplePage.radioButton).click();
         cy.get(samplePage.checkbox).click();
-        cy.get('select').select('1');
         cy.get(samplePage.datepicker).type('01/01/2022');
         cy.get(samplePage.submitButton).click();
         cy.get(samplePage.successMessage).should('contain', 'The form was successfully submitted!');
