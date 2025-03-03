55dcac57b451ec9c3fcc720883fb15c3306dd417
orhunakkan <orhunakkan@outlook.com>
2025-03-03 11:29:40 -0600

    Remove obsolete registration form and API utility files; add new registration form test file and update .gitignore for reports

--- pages/apiEndpoints.ts
+++
@@ -1,9 +0,0 @@
-export const ApiEndpoints = {
-  baseUrl: 'https://reqres.in/api',
-  users: '/users',
-  singleUser: (id: number) => `/users/${id}`,
-  resources: '/unknown',
-  singleResource: (id: number) => `/unknown/${id}`,
-  register: '/register',
-  login: '/login'
-};
